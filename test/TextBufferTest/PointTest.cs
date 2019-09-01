31fc5402b28124fb252595421a56a807200064b0
Chad Kirby <ckirby@biddle.com>
2019-08-31 17:00:38 -0700

    Added new Point tests

--- test/TextBufferTest/PointTest.cs
+++ test/TextBufferTest/PointTest.cs
@@ -30,5 +30,40 @@ namespace TextBufferTest
             Assert.Same(origin, Point.FromObject(origin, false));
             Assert.NotSame(origin, Point.FromObject(origin, true));
         }
+
+        [Fact]
+        public void CopyReturnsACopyOfTheObject()
+        {
+            Assert.Equal(new Point(3, 4), new Point(3, 4).Copy());
+            Assert.Equal(new Point(0, 0), Point.Zero.Copy());
+        }
+
+        [Fact]
+        public void NegateReturnsANewPointWithRowAndColumnNegated()
+        {
+            Assert.Equal(new Point(-3, -4), new Point(3, 4).Negate());
+            Assert.Equal(new Point(0, 0), Point.Zero.Negate());
+        }
+
+        [Fact]
+        public void FreezeMakesThePointObjectImmutable()
+        {
+            Assert.Throws<InvalidOperationException>(() => new Point(3, 4).Freeze().Row = 5);
+            Assert.Throws<InvalidOperationException>(() => new Point(3, 4).Freeze().Column = 6);
+            Assert.Throws<InvalidOperationException>(() => Point.Zero.Freeze().Row = 1);
+            Assert.Throws<InvalidOperationException>(() => Point.Zero.Freeze().Column = 2);
+        }
+
+        [Theory]
+        [InlineData(-1, 2, 3, 2, 6)]
+        [InlineData(-1, 2, 3, 3, 4)]
+        [InlineData(0, 1, 1, 1, 1)]
+        [InlineData(1, 2, 3, 2, 0)]
+        [InlineData(1, 2, 3, 1, 3)]
+        public void CompareToReturnsNegOneForLessThanZeroForEqualToOneForGreaterThanComparisons(
+            int expected, double rowA, double columnA, double rowB, double columnB)
+        {
+            Assert.Equal(expected, new Point(rowA, columnA).CompareTo(new Point(rowB, columnB)));
+        }
     }
 }
